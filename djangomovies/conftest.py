696a8d487bae6d19499d9948ca7e87960f62d6bc
Lydiah <lydiahcedric@gmail.com>
2023-11-08 17:06:20 +0300

    generate cookiecutter Django project

---
+++ djangomovies/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from djangomovies.users.models import User
+from djangomovies.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
